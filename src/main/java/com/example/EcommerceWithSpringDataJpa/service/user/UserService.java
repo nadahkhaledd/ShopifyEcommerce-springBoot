4e08a11f8290c843cf8bed84c24824768095169a
Nadah Khaled <nadahkhaledd@gmail.com>
2022-12-20 15:46:30 +0200

    - implement UserService

---
+++ src/main/java/com/example/EcommerceWithSpringDataJpa/service/user/UserService.java
@@ -0,0 +1,47 @@
+package com.example.EcommerceWithSpringDataJpa.service.user;
+
+import com.example.EcommerceWithSpringDataJpa.entity.User;
+import com.example.EcommerceWithSpringDataJpa.exception.UserNotFoundException;
+import com.example.EcommerceWithSpringDataJpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements IUserService{
+
+    private final UserRepository userRepo;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepo,
+                       UserRepository userRepository){
+        this.userRepo = userRepo;
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getByID(Integer id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException(email));
+    }
+
+    @Override
+    public String getUserNameByID(Integer id) {
+        return userRepository.findUserNameByID(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+}
