6e677f50609d1d7233488aa73c4bd740503759b0
Nadah Khaled <nadahkhaledd@gmail.com>
2022-12-20 18:39:14 +0200

    - implement AdminService

---
+++ src/main/java/com/example/EcommerceWithSpringDataJpa/service/admin/IAdminService.java
@@ -0,0 +1,25 @@
+package com.example.EcommerceWithSpringDataJpa.service.admin;
+
+import com.example.EcommerceWithSpringDataJpa.dto.UpdateUserDTO;
+import com.example.EcommerceWithSpringDataJpa.entity.Admin;
+
+import java.util.List;
+
+public interface IAdminService {
+
+    Admin save(Admin admin);
+
+    Admin getByID(Integer id);
+
+    Admin getByEmail(String email);
+
+    Admin update(Integer id, UpdateUserDTO admin);
+
+    List<Admin> getAll();
+
+    void delete(Admin admin);
+
+    void deleteByID(Integer id);
+
+    void deactivateCustomer(String email);
+}
