ef521e4bca5a21d616e4d65c3bece96f38d0e2eb
Nadah Khaled <nadahkhaledd@gmail.com>
2022-12-20 15:45:55 +0200

    - modify application.yaml & add on-startup data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO `ecommerce`.`user`
+(`user_type`,
+`date_of_birth`,
+`email`,
+`first_name`,
+`gender`,
+`last_name`,
+`password`,
+`password_attempts`,
+`status`)
+VALUES(0, "1989-10-13", "superadmin@shopify.com", "super", 0,"admin", "super@dm1n", 0, 1)
